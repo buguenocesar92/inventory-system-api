80d459ba05241ee5cc57c9414c14a35496866f2c
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-04 16:05:34 -0300

    Add Spatie Laravel Permission package and implement role management in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
-/* use Spatie\Permission\Traits\HasRoles; */
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable/* , HasRoles */;
+    use HasFactory, Notifiable, HasRoles ;
 
     protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token'];
 
     public function getJWTIdentifier()
     {
