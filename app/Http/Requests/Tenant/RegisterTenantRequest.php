20426131f48e1dc9cfcd8c49f386539538e6644e
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-07 19:32:01 -0300

    Add RegisterTenantRequest class for validating tenant registration input

---
+++ app/Http/Requests/Tenant/RegisterTenantRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Tenant;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterTenantRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'tenant_id' => [
+                'required',
+                'string',
+                'regex:/^[a-zA-Z0-9\-]+$/',
+                'unique:tenants,id',
+            ],
+            'user_name' => 'required|string|max:255',
+            'user_email' => 'required|email|unique:users,email',
+            'user_password' => 'required|string|min:8',
+        ];
+    }
+}
