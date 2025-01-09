0640f76f3520f25309d1e94a0d8b5e9bd9a2b59d
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-09 14:17:17 -0300

    Add PHPDoc comments for UserRepository class and create method

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repositories;
 
 use App\Models\User;
-
+/**
+ * Repositorio para manejar operaciones de usuarios.
+ */
 class UserRepository
 {
     private User $model;
@@ -12,7 +14,12 @@ class UserRepository
     {
         $this->model = $model;
     }
-
+    /**
+     * Crear un usuario.
+     *
+     * @param array $data Datos del usuario.
+     * @return User Usuario creado.
+     */
     public function create(array $data): User
     {
         return $this->model->create($data);
