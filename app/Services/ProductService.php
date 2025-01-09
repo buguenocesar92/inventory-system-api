499a92fe596c0b0c29bed3e61d41af53cbb51814
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-09 17:59:14 -0300

    Add ProductService for managing product-related operations

---
+++ app/Services/ProductService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\ProductRepository;
+use App\Models\Product;
+
+class ProductService
+{
+    private ProductRepository $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->productRepository->getAll();
+    }
+
+    public function find(Product $product): Product
+    {
+        return $this->productRepository->find($product);
+    }
+
+    public function create(array $data): Product
+    {
+        return $this->productRepository->create($data);
+    }
+
+    public function update(Product $product, array $data): Product
+    {
+        return $this->productRepository->update($product, $data);
+    }
+
+    public function delete(Product $product): void
+    {
+        $this->productRepository->delete($product);
+    }
+}
