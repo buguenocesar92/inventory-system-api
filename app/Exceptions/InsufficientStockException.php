5317590e60fe1fc2df49f5ee65193fe7cfe7d678
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-09 15:33:50 -0300

    Add InsufficientStockException for handling stock-related errors

---
+++ app/Exceptions/InsufficientStockException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class InsufficientStockException extends Exception
+{
+    protected $message = 'Insufficient stock';
+
+    public function __construct(string $message = null)
+    {
+        parent::__construct($message ?? $this->message);
+    }
+
+    public function render(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(['error' => $this->getMessage()], 400);
+    }
+}
