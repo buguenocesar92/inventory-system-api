1ffbb4468ec071b35ce1e696a3fe0327b4448aca
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-13 18:23:18 -0300

    Add SaleRepository for managing sales creation

---
+++ app/Repositories/SaleRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Sale;
+
+class SaleRepository
+{
+    public function create(array $data): Sale
+    {
+        return Sale::create([
+            'product_id'  => $data['product_id'],
+            'quantity'    => $data['quantity'],
+            'unit_price'  => $data['unit_price'],
+            'total_price' => $data['total_price'],
+        ]);
+    }
+}
