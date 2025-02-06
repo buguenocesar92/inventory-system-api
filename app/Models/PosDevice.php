25a5006eeb940343b62c2d42ce82882cc8e9ed6d
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-06 17:05:54 -0300

    feat: add PosDevice model with relationships to Location and CashRegister

---
+++ app/Models/PosDevice.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PosDevice extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'location_id',
+        'identifier',
+        'status',
+    ];
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function cashRegisters()
+    {
+        return $this->hasMany(CashRegister::class);
+    }
+}
