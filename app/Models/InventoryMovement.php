c8a233cef3b5f40851ce7645d9ed870cf8b1f2ec
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-06 17:05:50 -0300

    feat: add location_id field to InventoryMovement model and establish relationship with Location

--- app/Models/InventoryMovement.php
+++ app/Models/InventoryMovement.php
@@ -10,11 +10,17 @@ class InventoryMovement extends Model
     use HasFactory;
 
     protected $fillable = [
-        'product_id', 'movement_type', 'quantity', 'description'
+        'product_id', 'movement_type', 'quantity', 'description', 'location_id'
     ];
 
     public function product()
     {
         return $this->belongsTo(Product::class);
     }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
 }
