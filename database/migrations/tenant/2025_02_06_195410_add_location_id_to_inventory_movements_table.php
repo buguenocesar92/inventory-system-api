6083e3c19e0f3a76f93717926a3a174911a35712
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-06 17:06:12 -0300

    feat: add location_id field to InventoryMovements table and establish foreign key relationship with Locations

---
+++ database/migrations/tenant/2025_02_06_195410_add_location_id_to_inventory_movements_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up() {
+        Schema::table('inventory_movements', function (Blueprint $table) {
+            $table->unsignedBigInteger('location_id')->after('id');
+            $table->foreign('location_id')->references('id')->on('locations')->onDelete('cascade');
+        });
+    }
+
+    public function down() {
+        Schema::table('inventory_movements', function (Blueprint $table) {
+            $table->dropForeign(['location_id']);
+            $table->dropColumn('location_id');
+        });
+    }
+};
+
