9e15799762c085d37896877b2446c6a815103d56
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-07 15:03:04 -0300

    feat: create Warehouse model with relationships to Location and ProductStock

---
+++ app/Models/Warehose.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Warehouse extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['location_id', 'name', 'type'];
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function productStock()
+    {
+        return $this->hasMany(ProductStock::class);
+    }
+}
