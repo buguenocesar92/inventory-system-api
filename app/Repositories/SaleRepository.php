ff3bdf7a12f6e4b5e097ea9784cc8dba135fb81f
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-07 19:01:46 -0300

    feat: extend SaleRepository create method to include warehouse and pos device information

--- app/Repositories/SaleRepository.php
+++ app/Repositories/SaleRepository.php
@@ -11,11 +11,13 @@ class SaleRepository
         return Sale::create([
             'product_id'       => $data['product_id'],
             'user_id'          => $data['user_id'],
-            'cash_register_id' => $data['cash_register_id'], // Añade este campo
-            'location_id'      => $data['location_id'], // Añade este campo
             'quantity'         => $data['quantity'],
             'unit_price'       => $data['unit_price'],
             'total_price'      => $data['total_price'],
+            'cash_register_id' => $data['cash_register_id'],
+            'location_id'      => $data['location_id'],
+            'warehouse_id'     => $data['warehouse_id'],
+            'pos_device_id'    => $data['pos_device_id'],
         ]);
     }
 }
