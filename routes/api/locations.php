f3649fa1c47bc20678a5dbfc79cee49bf8237590
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-09 16:36:40 -0300

    feat: add API routes for managing locations with authentication and permissions

---
+++ routes/api/locations.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
+
+Route::group([
+    'prefix' => 'locations',
+    'middleware' => 'auth:api',
+], function () {
+    Route::get('/', [LocationController::class, 'index'])->name('locations.index');
+    Route::post('/', [LocationController::class, 'store'])->middleware('permission:locations.store');
+    Route::get('/{id}', [LocationController::class, 'show'])->middleware('permission:locations.show');
+    Route::put('/{id}', [LocationController::class, 'update'])->middleware('permission:locations.update');
+    Route::delete('/{id}', [LocationController::class, 'destroy'])->middleware('permission:locations.destroy');
+});
