940e4366faa071d4e225a7392d4e1c50b5b45e19
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-09 16:58:53 -0300

    feat: add permission middleware to locations index route

--- routes/api/locations.php
+++ routes/api/locations.php
@@ -7,7 +7,7 @@ Route::group([
     'prefix' => 'locations',
     'middleware' => 'auth:api',
 ], function () {
-    Route::get('/', [LocationController::class, 'index'])->name('locations.index');
+    Route::get('/', [LocationController::class, 'index'])->middleware('permission:locations.index');
     Route::post('/', [LocationController::class, 'store'])->middleware('permission:locations.store');
     Route::get('/{id}', [LocationController::class, 'show'])->middleware('permission:locations.show');
     Route::put('/{id}', [LocationController::class, 'update'])->middleware('permission:locations.update');
